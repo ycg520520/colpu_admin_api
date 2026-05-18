f3457d04fac9a59e67d6938fa62ed8af5fbef4a5
colpu <ycg520520@qq.com>
2026-05-18 17:41:47 +0800

    fix

--- scripts/ai.js
+++ scripts/ai.js
@@ -2,7 +2,7 @@
  * @Author: colpu
  * @Date: 2026-03-24 16:52:46
  * @LastEditors: colpu ycg520520@qq.com
- * @LastEditTime: 2026-05-18 17:39:18
+ * @LastEditTime: 2026-05-18 17:41:36
  * @
  * @Copyright (c) 2026 by colpu, All Rights Reserved.
  */
@@ -25,8 +25,10 @@ async function initData(isSync) {
     await classify.create(item);
   }
   for (const item of (await import('./data/ai/template.js')).default) {
-    if (!!item.prompt_variables) {
+    if (item.prompt_variables) {
       item.prompt_variables = JSON.stringify(item.prompt_variables);
+    } else {
+      item.prompt_variables = null;
     }
     await template.create(item);
   }
